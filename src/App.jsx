8507af2af305dd060c6d0ba6fae7bac7ffbe8390
SMIITT22 <prosmit2707@gmail.com>
2024-09-30 18:22:07 +0200

    add fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import "./App.css";
-
-function App() {
-  return <div>Hi</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="font-gloria text-4xl">
+          This is a Gloria Hallelujah Heading
+        </h1>
+        <p className="font-poppins text-lg">
+          This is some text in Poppins font.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ import withMT from "@material-tailwind/react/utils/withMT";
 export default withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        gloria: ["Gloria Hallelujah", "cursive"],
+      },
+    },
   },
   plugins: [],
 });
